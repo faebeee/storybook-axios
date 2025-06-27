3c13e68524efb8df143a50857861288850759a7f
Stefan Bartos <stefan.svinciak@gmail.com>
2025-06-27 10:06:52 +0200

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,19 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "../register"
-  ]
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 }
